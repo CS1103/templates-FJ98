7955c9b2b5b5084f457d4e901246ba06efba5aa7
FJ98 <felix.solano@utec.edu.pe>
2019-05-17 11:58:07 -0500

    QuickSort Template

---
+++ QUICKSORT_TEMPLATE/cmake-build-debug/CMakeFiles/QUICKSORT_TEMPLATE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QUICKSORT_TEMPLATE.dir/main.cpp.o"
+  "QUICKSORT_TEMPLATE.pdb"
+  "QUICKSORT_TEMPLATE.exe"
+  "libQUICKSORT_TEMPLATE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QUICKSORT_TEMPLATE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
