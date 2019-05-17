2241a5363410097ab2a3d4e1fd8964c391b52ead
FJ98 <felix.solano@utec.edu.pe>
2019-05-17 11:59:46 -0500

    QuickSort Template

--- QUICKSORT_TEMPLATE/main.cpp
+++ QUICKSORT_TEMPLATE/main.cpp
@@ -3,14 +3,14 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    const int SIZE = 10;
-    int array[SIZE] = {10, 4, 7, 5, 8, 1, 6, 9, 3, 2};
+    const int TAMANO = 10;
+    int array[TAMANO] = {10, 4, 7, 5, 8, 1, 6, 9, 3, 2};
 
     for (int item : array){
         std::cout << item << " ";
     }std::cout << std::endl;
 
-    quickSort(array, 0, SIZE-1);
+    quickSort(array, 0, TAMANO-1);
 
     for (int item : array){
         std::cout << item << " ";
